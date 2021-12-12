ae1ecf7d0f02cdff5f42b9e98d44ca6f7a8e8c4a
Pavel Petkov <elias@5247698@abv.bg>
2021-12-12 02:16:42 +0200

    making nav and styles for nav and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import Login  from "./components/Login";
+import  NavBar  from "./components/Navbar";
 import "./styles/App.css"
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Login/>
     </div>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
+import "../styles/Navbar.css"
 
-
-function NavBar({isAuthenticated}){
+export default function Navbar({isAuthenticated}){
     return (
-        <nav>
-            {isAuthenticated} ? <>
+        <nav className="Navbar">
+            <a href="/profile">Chatter</a>
+            <div>
+            {isAuthenticated ? <>
                 <a href="/profile">My Profile</a>
                 <a href="/friends">My Friends</a>
-                <a href="#">Logout</a>
+                <a href="/">Logout</a>
             </> : <>
                 <a href="/login">Login</a>
                 <a href="/register">Register</a>
-            </>
+            </>}
+            </div>
         </nav>
     )
 }

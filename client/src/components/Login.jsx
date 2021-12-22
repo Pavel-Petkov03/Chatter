bdcfad754c2cc758db714a75dc90017c600df42e
Pavel Petkov <elias@abv.bg>
2021-12-22 22:40:51 +0200

    making changes on login and register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,15 @@
 import "../styles/LoginAndRegister.css"
 export default function Login(){
     return (
-        <div className="Register">
+        <div className="Login">
             <h1>Login</h1>
             <form action="" className="form">
                 <input type="text" placeholder="Email"/>
                 <input type="text" placeholder="Password"/>
-               <button>Submit</button>
+               <button >Submit</button>
             </form>
         </div>
     )
 }
+
+
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import "../styles/Navbar.css"
-
+import {Link} from "react-router-dom"
 export default function Navbar({isAuthenticated}){
     return (
-        <nav className="Navbar">
-            <a href="/profile">Chatter</a>
-            <div>
-            {isAuthenticated ? <>
-                <a href="/profile">My Profile</a>
-                <a href="/friends">My Friends</a>
-                <a href="/">Logout</a>
-            </> : <>
-                <a href="/login">Login</a>
-                <a href="/register">Register</a>
-            </>}
-            </div>
-        </nav>
+            <nav className="Navbar">
+                <a href="/profile">Chatter</a>
+                <div>
+                    {isAuthenticated ? <>
+                    <a href="/profile">My Profile</a>
+                    <a href="/friends">My Friends</a>
+                    <a href="/">Logout</a>
+                </> : <>
+                <Link to="register">Register</Link>
+                <Link to="login">Login</Link>
+                </>
+                }
+                </div>
+            </nav>
     )
 }

8256284597b2fabb834fb6c692edefd6e5d29c0d
Pavel Petkov <elias5247698@abv.bg>
2021-12-23 22:11:04 +0200

    added emoji picker and add some icons

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,12 +1,15 @@
 import "../styles/Post.css"
 import { useState, useEffect } from "react"
-import {FaRegCommentAlt, FaHeart, FaRegHeart} from "react-icons/fa"
+import {FaRegCommentAlt, FaHeart, FaRegHeart, FaArrowCircleRight, FaRegLaughBeam} from "react-icons/fa"
+import Picker from "emoji-picker-react"
 export default function Post({
     userImage, username, postImg , content, 
 }){
     const [isCommentClicked, setIsCommentClicked] = useState(false)
     const [isGray , setIsGray] = useState(false)
+    // this will be setted by props
     const [liked , setIsLiked] = useState(false)
+    const [isOpenEmojiClicked , setIsOpenEmojiClicked] = useState(false)
     let Icon
     const currentImg = userImage ? userImage  : "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
     return (
@@ -23,14 +26,24 @@ export default function Post({
                         <FaRegCommentAlt color={isGray ? "gray" : "black"} onClick={() => {
                             setIsCommentClicked(last => !last)
                             setIsGray(gray => !gray)
+                            setIsOpenEmojiClicked(bool => false)
                         }} />
                         {Icon = liked ? FaHeart : FaRegHeart}
                         <Icon color={liked ? "red" : "black"} onClick={() => setIsLiked(l => !l )}/> 
                     </div>
                  </div>
             </div>
+            {isOpenEmojiClicked ? <Picker pickerStyle={{position : "absolute",  margin : "60px 270px"}} /> : null}
             {isCommentClicked ? <>
-            <div><textarea className="comment-create"/></div>
+            <div className="comment-create-section">
+                <textarea className="comment-create"/>
+                
+                <div className="post-tasks">
+                    <FaRegLaughBeam  onClick={() => setIsOpenEmojiClicked(e => !e)}/>
+                    <FaArrowCircleRight className="post-comment"/>
+                </div>
+                
+            </div>
                 {/* todo improve post comment design */}
             </> : null}
         </article>

9e62ca24eb752cc663acee442c9ebf9513ccf14d
Pavel Petkov <elias5247698@abv.bg>
2021-12-23 20:25:35 +0200

    making implementation of icons

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,9 +1,13 @@
 import "../styles/Post.css"
-
-
+import { useState, useEffect } from "react"
+import {FaRegCommentAlt, FaHeart, FaRegHeart} from "react-icons/fa"
 export default function Post({
     userImage, username, postImg , content, 
 }){
+    const [isCommentClicked, setIsCommentClicked] = useState(false)
+    const [isGray , setIsGray] = useState(false)
+    const [liked , setIsLiked] = useState(false)
+    let Icon
     const currentImg = userImage ? userImage  : "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
     return (
         <article className="post">
@@ -11,17 +15,26 @@ export default function Post({
                 <img src={currentImg} alt="" className="name-row-img"/>
                 <h5 className="name-row-name">{username}</h5>
             </div>
-            
+            <p className="post-content-text">{content}</p>
             <div className="post-content">
                 <img src={postImg} alt="" className="post-image"/>
                 <div className="post-footer">
-                    <p className="post-content-text">{content}</p>
                     <div className="post-buttons">
-                        <button>Like</button>
-                        <button>Comment</button>
+                        <FaRegCommentAlt color={isGray ? "gray" : "black"} onClick={() => {
+                            setIsCommentClicked(last => !last)
+                            setIsGray(gray => !gray)
+                        }} />
+                        {Icon = liked ? FaHeart : FaRegHeart}
+                        <Icon color={liked ? "red" : "black"} onClick={() => setIsLiked(l => !l )}/> 
                     </div>
                  </div>
             </div>
+            {isCommentClicked ? <>
+            <div><textarea className="comment-create"/></div>
+                {/* todo improve post comment design */}
+            </> : null}
         </article>
     )
 }
+
+

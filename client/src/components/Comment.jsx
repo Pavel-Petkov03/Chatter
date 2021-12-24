b251fa48e2bc1b992e1f38c7ddf4e198df13bdac
Pavel Petkov <elias5247698@abv.bg>
2021-12-25 01:41:28 +0200

    starting comments design

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,12 +1,17 @@
-
+import "../styles/Comment.css"
 export default function Comment({
     ownerId ,  content , ownerImage , ownerName
 }){
     // TODO MAKE CSS FOR COMMENTS 
     return (
-        <div className="comment" key={ownerId}>
-            <img src={ownerImage} alt="" />
-            <p>{ownerName}:{content}</p>
+        <li class="comment-object">
+        <div class="image-container">
+          <img class="comment-profile-pick" src="https://pbs.twimg.com/profile_images/675393797799460865/N58WAkoH.jpg" alt="profile picture"/>
         </div>
+        <div class="comment-text">
+          <h2 class="username">Obi Wan Kenobi <span class="muted">&#183; 3 hours ago</span></h2>
+          <p class="comment">Think Anakin might be just a bit evil </p>
+        </div>
+      </li>
     )
 }

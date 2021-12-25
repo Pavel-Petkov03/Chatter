a9a1414bb43d4ac7cae645ec50d8b5e6c01f48b6
Pavel Petkov <elias@abv.bg>
2021-12-25 10:02:20 +0200

    making design for comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,13 +5,16 @@ export default function Comment({
     // TODO MAKE CSS FOR COMMENTS 
     return (
         <li class="comment-object">
-        <div class="image-container">
-          <img class="comment-profile-pick" src="https://pbs.twimg.com/profile_images/675393797799460865/N58WAkoH.jpg" alt="profile picture"/>
-        </div>
         <div class="comment-text">
-          <h2 class="username">Obi Wan Kenobi <span class="muted">&#183; 3 hours ago</span></h2>
+          <div className="comment-let-content">
+            <div className="comment-profile-info">
+              <img class="comment-profile-pick" src="https://pbs.twimg.com/profile_images/675393797799460865/N58WAkoH.jpg" alt="profile picture"/>
+              <h2 class="username">Obi Wan Kenobi </h2>
+            </div>
           <p class="comment">Think Anakin might be just a bit evil </p>
+          </div>
         </div>
+        <span class="muted">&#183; 3 hours ago</span>
       </li>
     )
 }

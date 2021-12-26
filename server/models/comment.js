a3d1d2017b283c930de467e1d46962b47e9c2cd2
Pavel Petkov <elias5247698@abv.bg>
2021-12-26 22:15:57 +0200

    finishing comments, posts and user models

---
+++ server/models/comment.js
@@ -0,0 +1,12 @@
+import {mongoose} from "../config/mongoose.js"
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+    content : String,
+    ownerImg : String,
+    likesCount : {type : Number, default : 0},
+});
+
+
+
+module.exports = mongoose.model('Comment', commentSchema);

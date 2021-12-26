2a551a69e03223761a00a6a069e0147329ac8733
Pavel Petkov <elias@abv.bg>
2021-12-26 22:15:57 +0200

    finishing comments, posts and user models

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/chatter").then(() => {
+mongoose.connect(env.process.MONGODB_CONNECTION_STRING).then(() => {
     console.log("CONNECTED TO DATABASE SUCCESSFULLY")
 }).catch(er => {
     console.log(er.message)

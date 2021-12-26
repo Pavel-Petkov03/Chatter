66dc9022531dbaafeffecacd40f6bb7474d8ea87
Pavel Petkov <elias5247698@abv.bg>
2021-12-26 22:09:48 +0200

    starting database implementation and connection

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   email: String,
   username: String,
   password: String,
+  profilePicture : String // this will be changed to some cloud like firebase or cloudinary
 });
 
 userSchema.index(

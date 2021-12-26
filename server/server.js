df4705d944138dd047c1254fac2cb4a79ba9410c
Pavel Petkov <elias@abv.bg>
2021-12-27 00:20:22 +0200

    making connection to mongo db with cluster

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require("express")
-
 const app = express()
+const router = require("./routes/homePage.js")
 
-
-
+app.use("/", router)
 app.listen(5000, () => {
     console.log("Listening on post 5000")
 })

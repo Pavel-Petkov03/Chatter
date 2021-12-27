695af2d35ae32b45584ebcba9df06a62cee31490
Pavel Petkov <elias@abv.bg>
2021-12-27 03:31:50 +0200

    creating models and server and adding css for comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,20 +1,29 @@
 import "../styles/Comment.css"
+import CustomHeart from "./CustomHeart"
 export default function Comment({
-    ownerId ,  content , ownerImage , ownerName, postedDate
+  
+    ownerId ,  content , ownerImage , ownerName, postedDate, likesCount
 }){
     // TODO MAKE CSS FOR COMMENTS 
     return (
         <section class="comment-placeholder" key={ownerId}>
-        <article class="comment-text">
-          <div className="comment-let-content">
             <div className="comment-profile-info">
-              <img class="comment-profile-pick" src={ownerImage} alt="profile picture"/>
-              <h2 class="username">{ownerName}</h2>
+              <div className="comment-first-row">
+                <div className="comment-profile-info-box">
+                  <img class="comment-profile-pick" src={ownerImage} alt="profile picture"/>
+                  <h2 class="username">{ownerName}</h2>
+                </div>
+                <span >{postedDate}</span>
+              </div>
+              <div className="comment-second-row">
+                <p class="comment-content">{content}</p>
+                <div>
+                  <p>Likes: {likesCount}</p>
+                  <CustomHeart likedBool={false}></CustomHeart>
+                </div>
+              </div>
+              
             </div>
-          <p class="comment">{content}</p>
-          </div>
-        </article>
-        <span >{postedDate}</span>
       </section>
     )
 }
--- server/routes/homePage.js
+++ server/routes/homePage.js
@@ -5,7 +5,7 @@ const router = require("express").Router()
 router.get("/", (req , res) => {
     const skip =  0
     const limit = 10
-    Post.sort({creationDate : -1}).limit(limit).skip(skip).exec((error , data) => {
+    Post.find().sort({creationDate : -1}).limit(limit).skip(skip).exec((error , data) => {
         if(error){
             console.log(error)
         }else{
@@ -14,6 +14,23 @@ router.get("/", (req , res) => {
                 posts : data
             })
         }
+    })
+})
+
+
+router.post("/", async (req, res) => {
+    console.log(req.body)
+    const {content , userImage, postImage } = req.body
+    // the pictures will be saved in cloud later
+    // THIS IS ONLY FOR DEBUGGING
+    const post = new Post({
+        content,
+        userImage,
+        postImage
+    })
+    await post.save()
+    res.status(200).json({
+        message : "Post is created successfully"
     })
 })
 

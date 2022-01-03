d7c5b56bed71c475609d27f4322bcd0a0dfecd3e
Pavel Petkov <elias@5247698@abv.bg>
2022-01-03 12:35:42 +0200

    setting all states in useState hooks and then going to add redux

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
 const router = require("./routes/homePage.js")
 app.use(express.json())
+app.use(cookieParser())
 app.use("/", router)
 app.listen(5000, () => {
     console.log("Listening on post 5000")

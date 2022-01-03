ac6c27a5586df0056e4ec818e9667a3e056aecea
Pavel Petkov <elias@abv.bg>
2022-01-03 21:45:57 +0200

    getting started with redusers

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -96,5 +96,3 @@ export default function Post({
         </article>
     )
 }
-
-
---
+++ client/src/reducers/posts/actionTypes.js
@@ -0,0 +1,11 @@
+// this file will only export strings
+
+export const CREATE_POST_SUCCESS = "Create Post Success"
+export const EDIT_POST_SUCCESS = "Create Post Success"
+export const LIKE_POST_SUCCESS = "Like Post Success"
+export const SAVE_POST_SUCCESS = "Save Post Success"
+
+export const CREATE_POST_FAILURE = "Create Post Failure"
+export const EDIT_POST_FAILURE = "Create Post Failure"
+export const LIKE_POST_FAILURE = "Like Post Failure"
+export const SAVE_POST_FAILURE = "Save Post Failure"

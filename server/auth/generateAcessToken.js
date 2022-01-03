61550fdb1f2d03f335d94762fdfc77d324c65d07
Pavel Petkov <elias5247698@abv.bg>
2022-01-03 12:35:42 +0200

    setting all states in useState hooks and then going to add redux

--- server/auth/generateAcessToken.js
+++ server/auth/generateAcessToken.js
@@ -1,22 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 
-function verifyToken(req , res , next){
-    const token = req.headers.authorization.split(" ")[1]
-    try{
-        if(!token){
-            res.status(401).json({
-                message : "Not authorized"
-            })
-        }
-        jwt.verify(token , env.process.ACCESS_TOKEN_KEY)
-    }catch(er){
-        return res.json({
-            message : "Not authorized"
-        })
-    }
-    next()
-}
+
 
 
 module.exports = {

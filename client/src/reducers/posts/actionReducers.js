066df869efefc2f93b8b382a5afee4933f044803
Pavel Petkov <elias5247698@abv.bg>
2022-01-07 23:45:00 +0200

    finishing post reducers

--- client/src/reducers/posts/actionReducers.js
+++ client/src/reducers/posts/actionReducers.js
@@ -1,14 +1,22 @@
 import {
+    CREATE_COMMENT_FAILURE,
     CREATE_COMMENT_SUCCESS,
+    DELETE_POST_FAILURE,
     DELETE_POST_SUCCESS, 
+    EDIT_POST_FAILURE, 
     EDIT_POST_SUCCESS,
+    LIKE_POST_FAILURE,
+    LIKE_POST_SUCCESS,
+    SAVE_POST_FAILURE,
     SAVE_POST_SUCCESS, 
 } 
 from "./actionTypes.js"
 
+const commentPaginationCount =  2
 
 function postReducer(state , action){
     switch (action.type){
+        // if correct api call the state is reseted
         case CREATE_COMMENT_SUCCESS :
         case EDIT_POST_SUCCESS :
         case DELETE_POST_SUCCESS :
@@ -18,6 +26,16 @@ function postReducer(state , action){
                 clickedComment : false , 
                 isEmojiFieldClicked : false,
                 editMode : false
+            }
+        
+        case CREATE_COMMENT_FAILURE : 
+        case DELETE_POST_FAILURE : 
+        case EDIT_POST_FAILURE : 
+        case SAVE_POST_FAILURE :
+        case LIKE_POST_FAILURE :
+            return {
+                ...state ,
+                errorMessage : action.errorMessage
             }
 
         case EDIT_POST:
@@ -31,19 +49,48 @@ function postReducer(state , action){
                 ...state,
                 clickedComment : true
             }
+        
+        case LIKE_POST_SUCCESS :
+            return {
+                ...state,
+                isLiked : true
+            }
+        case SHOW_DOWN : 
+            return {
+                ...state ,
+                ...showDownAndUpChecker(state.commentsArray , action.allComments , "down")
+            }
+        case SHOW_UP : 
+            return {
+                ...state ,
+                ...showDownAndUpChecker(state.commentsArray , action.allComments , "up")
+            }
     }
 }
 
 
 
-const initialState = {
-    editMode : false,
-    errorMessage : "",
-    clickedComment : false,
-    commentsArray : comments.slice(0,commentPaginationCount),
-    displayShowDown : comments.length > commentPaginationCount,
-    displayShowUp : false,
-    commentsCountLeft : comments.length - commentsArray.length,
-    isEmojiFieldClicked : false,
-    isLiked : false
+
+function showDownAndUpChecker(stateComments , allComments , upOrDown){
+    let commentsArray
+    let displayShowDown = true
+    let displayShowUp = true
+    if(upOrDown === "down"){
+        commentsArray = allComments.slice(0 , stateComments.length + commentPaginationCount)
+    }else if(upOrDown == "up"){
+        commentsArray = allComments.slice(0 , stateComments.length - commentPaginationCount)
+    }
+
+
+    if(commentsArray.length <= 2){
+        displayShowUp = false
+    }else if(commentsArray.length === allComments.length){
+        displayShowDown = false
+    }
+    return {
+        commentsArray,
+        displayShowUp,
+        displayShowDown
+    }
 }
+
--- client/src/reducers/posts/actionTypes.js
+++ client/src/reducers/posts/actionTypes.js
@@ -1,15 +1,25 @@
-// this file will only export strings
 
 
 
 
+// these constant are only for toggle buttons
+export const SHOW_DOWN = "Show Down"
+export const SHOW_UP = "Show Up"
+export const EDIT_POST = "Edit Post"
+export const CLICK_COMMENT = "Click Comment"
 
+
+
+// these constants are for api call without error
 export const CREATE_COMMENT_SUCCESS = "Create Comment Success"
 export const EDIT_POST_SUCCESS = "Edit Post Success"
 export const LIKE_POST_SUCCESS = "Like Post Success"
 export const SAVE_POST_SUCCESS = "Save Post Success"
 export const DELETE_POST_SUCCESS = "Delete Post Success"
 
+
+
+// these constants are for api call with error
 export const CREATE_COMMENT_FAILURE = "Create Comment Failure"
 export const EDIT_POST_FAILURE = "Create Post Failure"
 export const LIKE_POST_FAILURE = "Like Post Failure"

6e6c7f9668d8d5753f95841cf376d9e869a29456
Pavel Petkov <elias@5247698@abv.bg>
2022-01-08 23:09:04 +0200

    making comment api endpoints

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,8 +2,9 @@ const {mongoose} = require("../config/mongoose.js")
 const Schema = mongoose.Schema
 
 const commentSchema = new Schema({
+    ownerId : Schema.Types.ObjectId,
     content : String,
-    ownerImg : String,
+    ownerImg : {type : String , ref : "User"},
     likesCount : {type : Number, default : 0},
     details : {
         $ref : "Post",

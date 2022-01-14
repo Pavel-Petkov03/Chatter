547377d7af73de5b493ff102e8cf245f3b0606fa
Pavel Petkov <elias@5247698@abv.bg>
2022-01-14 22:28:44 +0200

    making login and register

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 // this function will validate tokens in backend
 function verifyToken(req, res, next) {
-  console.log(req.headers)
     try {
       const token = req.headers.authorization?.split(' ')[1];
       if (!token) {

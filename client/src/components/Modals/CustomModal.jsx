44b3877d298bd4fef5d9253d907ba1edb19a1afd
Pavel Petkov <elias@5247698@abv.bg>
2022-01-18 15:40:56 +0200

    making mapdispatch ptops drilling

--- client/src/components/Modals/CustomModal.jsx
+++ client/src/components/Modals/CustomModal.jsx
@@ -3,14 +3,14 @@ import "../../styles/Modal.css"
 
 export default function CustomModal(){
     return (
-        <div>
+        <div class="modal-container">
             <div class="modal">
                 <h1>Modal window</h1>
                 <button class="close-icon">&times;</button>
                 <p></p>
             <button class="close">Close</button>
             </div>
-            <div class="modal-container"></div>
         </div>
+            
     )
 }

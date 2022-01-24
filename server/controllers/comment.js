77b7ade27fc19b095d4cbf6eb365b789df3c590a
Pavel Petkov <elias5247698@abv.bg>
2022-01-24 23:35:56 +0200

    deleting comments model and maiking post model self esteem

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,11 +1,12 @@
 const Comment = require("../models/comment.js")
-
+const Post = require("../models/post.js")
 async function postComment(req , res){
-    const {content , ownerImg} = req.body
+    const {content , ownerImg, postId} = req.body
     try{
         const newComment = new Comment({
             content,
-            ownerImg
+            ownerImg,
+            postId
         })
         await newComment.save()
         return res.status(200).json({
@@ -19,7 +20,7 @@ async function postComment(req , res){
 }
 
 async function patchComment(req , res){
-    const commentId = req.params.commentId
+    const commentId = req.body.commentId
     Comment.findOne(commentId , async (er , comment) => {
         if(er){
             return res.status(404).json({
@@ -35,7 +36,7 @@ async function patchComment(req , res){
 }
 
 async function deleteComment(req, res) {
-    Comment.findByIdAndDelete(req.params.commentId , (er , data) => {
+    Comment.findByIdAndDelete(req.body.commentId , (er , data) => {
         if(er){
             return res.status(404).json({
                 errorMessage : er.message

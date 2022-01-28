7499aa940fb50e6b45731b504a36a756b8425d61
Pavel Petkov <elias@5247698@abv.bg>
2022-01-29 00:44:50 +0200

    making one commit log

--- client/src/reducers/posts/actionTypes.js
+++ client/src/reducers/posts/actionTypes.js
@@ -29,3 +29,28 @@ export const GET_POST_FAILURE = "Get Post Failure"
 export const CREATE_POST_FAILURE = "Create Post Failure"
 
 export const START_APPLICATION = "Start Application"
+
+
+
+
+// this will toggle edit comment mode
+export const EDIT_COMMENT = "Edit Comment"
+
+
+
+export const LIKE_COMMENT_SUCCESS = "Like Comment Success"
+
+// these will reset state in correct  api call
+export const DELETE_COMMENT_SUCCESS= "Delete Comment Success"
+export const EDIT_COMMENT_SUCCESS= "Edit Comment Success"
+
+
+//these will toggle error in api call error
+export const LIKE_COMMENT_FAILURE= "Like Comment Failure"
+export const DELETE_COMMENT_FAILURE= "Delete Comment Failure"
+export const EDIT_COMMENT_FAILURE= "Edit Comment Failure"
+
+
+export const SHOW_COMMENTS= "Show Comments"
+export const SHOW_DOWN = "Show Down"
+export const SHOW_UP = "Show Up"

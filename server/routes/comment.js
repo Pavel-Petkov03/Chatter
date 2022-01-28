a05e0be6bc4b94010274ed05892fc389230706d2
Pavel Petkov <elias5247698@abv.bg>
2022-01-28 23:24:57 +0200

    making routers for comments backend

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,3 +1,14 @@
 const commentsController = require("../controllers/comment.js")
+const express = require("express")
+const {verifyToken} = require("../middlewares/auth");
+
+const router = express.Router()
 
 
+router.post("/:postId" , verifyToken , commentsController.postComment)
+
+router.patch("/:postId/:commentId" , verifyToken , commentsController.patchComment)
+// patch has same validation logic as post
+
+
+router.delete("/:postId/:commentId" , verifyToken , commentsController.deleteComment)
